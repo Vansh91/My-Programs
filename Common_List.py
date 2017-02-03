debafee7f7c86abaa95d3e593e685c37d0d83507
Vanshdeep <coolx91@gmail.com>
2017-02-02 20:12:59 -0500

    Add files via upload

---
+++ Common_List.py
@@ -0,0 +1,14 @@
+def CommonList():
+    list1 = [2,1,2,3,4,5]
+    list2 = [2,3,4,7,8,9]
+    new = []
+
+    for i in list1:
+        if i in list2:
+            new.append(i)
+
+    s = set(new)    # A set contains a list of elements with no repeated values
+    print(s)
+    print(new)
+
+CommonList()
