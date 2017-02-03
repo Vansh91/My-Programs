e72e5c0fbc9970a7cacce0222b57e45668e09480
Vanshdeep <coolx91@gmail.com>
2017-02-02 20:27:46 -0500

    Update Max_Of_Three.py

--- Max_Of_Three.py
+++ Max_Of_Three.py
@@ -1,3 +1,5 @@
+# This program takes three numbers from a user and prints the maximum out of all
+
 num1 = int(input('Enter first number'))
 num2 = int(input('Enter second number'))
 num3 = int(input('Enter third number'))
