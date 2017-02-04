7be19c8fd34a6113026b5262d8939772b32a09f0
Vanshdeep <coolx91@gmail.com>
2017-02-03 20:34:15 -0500

    Update RandomWord_&_ReadFromFile.py

--- RandomWord_&_ReadFromFile.py
+++ RandomWord_&_ReadFromFile.py
@@ -1,21 +1,22 @@
-# 1. Picking a random word from a text file
+"""1. Picking a random word from a text file"""
 
 import random
 import sys
 
 def random():
-    # the open statement can have any text file, apparently it is 'sowpods.txt'
+    """the open statement can have any text file, apparently it is 'sowpods.txt' """
+    
     with open('sowpods.txt', 'r') as f:
-        # 'r' refers to "read given file".
+        """ 'r' refers to "read given file". """
         word = f.readline()
         print(random.choice(word).strip())
 
 random()
 
-# 2. Reading from a text file and appending it to a dictionary
+"""2. Reading from a text file and appending it to a dictionary"""
 
 dict = {}
-    # the open statement can have any text file, apparently it is 'Names.txt'
+    """ the open statement can have any text file, apparently it is 'Names.txt' """
 with open('Names.txt') as f:
     line = f.readlines()
     while line:
